810344f3c7bd56ebbd3d78835292cd562f52833c
cypherop <shubhampinjwani710@outlook.com>
2020-09-30 05:13:16 +0530

    hashmap

--- template.cpp
+++ template.cpp
@@ -1,7 +1,3 @@
-/*input
-
-*/
-
 #include <bits/stdc++.h>
 #include <unistd.h>
 #include <ext/pb_ds/assoc_container.hpp> 
@@ -9,8 +5,6 @@
 
 #define rep(i,j,n) for(i=j;i<n;i++)
 #define repi(i,j,n) for(i=j;i>n;i--)
-#define inf 1e18
-#define M 1000000007
 #define pie 3.141592653589793238
 #define ll long long
 #define ld long double
@@ -33,20 +27,38 @@ mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
 mt19937_64 rnd64(chrono::high_resolution_clock::now().time_since_epoch().count());
 
 ll power(ll a,ll m,ll mod){
-	ll ans=1;
+	ll ans = 1;
 	while(m){
-		if(m%2) ans*=a, ans%=(mod);
-		a=(a*a)%(mod);
-		m>>=1;
+		if(m%2) ans *= a, ans %= (mod);
+		a = (a*a) % mod;
+		m >>= 1;
 	}
 	return ans;
 }
 
+struct custom_hash {  // Credits: https://codeforces.com/blog/entry/62393
+ 	static uint64_t splitmix64(uint64_t x) {
+    	// http://xorshift.di.unimi.it/splitmix64.c
+    	x += 0x9e3779b97f4a7c15;
+    	x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+    	x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+    	return x ^ (x >> 31);
+  	}
+ 
+  	size_t operator()(uint64_t x) const {
+    	static const uint64_t FIXED_RANDOM =
+        	chrono::steady_clock::now().time_since_epoch().count();
+    	return splitmix64(x + FIXED_RANDOM);
+  	}
+  	size_t operator()(pair<int64_t, int64_t> Y) const {
+    	static const uint64_t FIXED_RANDOM =
+        	chrono::steady_clock::now().time_since_epoch().count();
+    	return splitmix64(Y.first * 31 + Y.second + FIXED_RANDOM);
+  	}
+};
 
-void init(ll* a,ll n,ll val){
-	for(ll i=0;i<n;i++)
-		a[i]=val;
-}
+const long long inf = 1e18;
+const long long M = 1000000007;
 
 void solve(){
 	ll i,j,_;
